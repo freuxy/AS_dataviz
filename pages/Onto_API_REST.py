c0a2e06e56988537a6e09da99c1ecd0927f8ecad
bsessou <bignon-boris-freuxy.sessou@etudiant.univ-rennes1.fr>
2024-12-01 19:13:18 +0100

    Connecting online Onto_API_REST

--- pages/Onto_API_REST.py
+++ pages/Onto_API_REST.py
@@ -1,8 +1,24 @@
 import streamlit as st
+import requests
+import duckdb
+import os
+import logging
+from init_db import init_db
 
 def display_onto_api():
     st.title("🌐 Ontology API REST")
     st.subheader("Interact with the REST API here.", divider=True)
+
+
+    if "data" not in os.listdir():
+        logging.error(os.listdir())
+        logging.error("creating data repository")
+        os.mkdir("data")
+
+    if "database.duckdb" not in os.listdir("data"):
+        init_db()
+
+    con = duckdb.connect(database="data/database.duckdb", read_only=False)
 
     user_input = st.text_input(
         label="Please enter your request id",
@@ -11,9 +27,33 @@ def display_onto_api():
     )
 
     validation=   st.button("View ancestors", type="primary")
-    if user_input:
-        if validation:
-            st.write(f"Vous avez saisi : {user_input}")
+    if user_input and validation:
+            #st.write(f"Vous avez saisi : {user_input}")
+
+            # URL de l'API que vous souhaitez appeler
+            api_url = f"https://as-test-api.onrender.com/?id={user_input}"
+            st.write(api_url)
+
+            # Appel de l'API REST
+            try:
+                response = requests.get(api_url)
+                if response.status_code == 200:
+                    # Si la requête réussit, afficher les résultats
+                    st.write("Résultats de l'API :")
+                    st.json(response.json())  # Affiche la réponse JSON
+                else:
+                    st.error(f"Erreur lors de la récupération des données. Code d'état : {response.status_code}")
+            except requests.exceptions.RequestException as e:
+                # Si une erreur de connexion ou autre se produit
+                st.error(f"Erreur lors de l'appel API : {e}")
+
+    onto_q = """
+            SELECT *
+            FROM onto
+            """
+    onto = con.execute(onto_q).df()
+    st.dataframe(onto)
+
 
     st.markdown("<div style='margin-bottom:24px;'></div>", unsafe_allow_html=True)
 
@@ -23,8 +63,7 @@ def display_onto_api():
     st.subheader("Information on using api", divider=True)
     st.markdown("""
     **Points de terminaison disponibles :**
-    - `/` : Obtenir des informations principales.
-    - `/get_parents` : Récupérer les relations parents-enfants.
+    - `/` : Récupérer les relations parents-enfants.
 
     **Exemple d'utilisation via `curl` :**
     ```bash
